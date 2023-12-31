b12740f0c7a9321f2abaae786e6ec1c629763165
didaryalcin <ddrylcn@gmail.com>
2023-12-31 18:57:30 +0300

    commit 1

---
+++ src/components/context.js
@@ -0,0 +1,10 @@
+/*
+context oluştur
+içine kullanmak istediklerini at
+kullanmak istediğin yerde kullanmak istediklerini kullan
+*/
+
+import { createContext } from "react";
+
+export const ProductContext = createContext();
+export const CartContext = createContext();
